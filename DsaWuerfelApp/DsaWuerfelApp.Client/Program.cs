9dceaf1fcc2c915155f5a245f8434d4b5455b38b
Bosko Uzelac <boskouzelac@gmail.com>
2026-03-23 11:39:59 +0100

    fixes for linux deployment

--- DsaWuerfelApp/DsaWuerfelApp.Client/Program.cs
+++ DsaWuerfelApp/DsaWuerfelApp.Client/Program.cs
@@ -1,18 +1,17 @@
-using System.Net.Http;
+using DsaWuerfelApp.Client;
+using DsaWuerfelApp.Client.Services;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using MudBlazor.Services;
-using DsaWuerfelApp.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSingleton<DsaWuerfelApp.Client.Services.GameClient>();
-builder.Services.AddScoped(_ => new HttpClient
-{
-    BaseAddress = new Uri("http://localhost:5206/")
-});
+builder.Services.AddSingleton<GameClient>();
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
 
