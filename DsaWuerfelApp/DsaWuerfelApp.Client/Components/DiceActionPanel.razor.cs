a71b1d3aa7a6a8612cb069f7eea7bf29fab234dd
Bosko Uzelac <boskouzelac@gmail.com>
2026-03-23 11:49:55 +0100

    Redesign Wuerfel page layout and implement attribute selection

---
+++ DsaWuerfelApp/DsaWuerfelApp.Client/Components/DiceActionPanel.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DsaWuerfelApp.Client.Components;
+
+public partial class DiceActionPanel
+{
+    [Parameter] public RenderFragment? ResultPillContent { get; set; }
+
+    [Parameter] public int Modifier { get; set; }
+
+    [Parameter] public EventCallback<int> ModifierChanged { get; set; }
+
+    [Parameter] public bool IsHiddenRoll { get; set; }
+
+    [Parameter] public EventCallback<bool> IsHiddenRollChanged { get; set; }
+
+    [Parameter] public bool HasSelection { get; set; }
+
+    [Parameter] public EventCallback OnRoll { get; set; }
+
+    private Task ToggleHiddenRoll()
+    {
+        IsHiddenRoll = !IsHiddenRoll;
+        return IsHiddenRollChanged.InvokeAsync(IsHiddenRoll);
+    }
+}
